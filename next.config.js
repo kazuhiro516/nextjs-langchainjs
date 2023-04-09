9849081bcf816cc24aaf94e6335f6cacd143bbed
hndrr <yozhik20@gmail.com>
2023-04-10 02:06:39 +0900

    package nameをrename

--- next.config.js
+++ next.config.js
@@ -1,39 +1,3 @@
-// const webpack = require("webpack");
-
-// module.exports = {
-//   reactStrictMode: true,
-//   webpack: (config, { isServer }) => {
-//     config.experiments = { ...config.experiments, topLevelAwait: true };
-//     config.externals['node:fs'] = 'commonjs node:fs';
-//     config.externals = {
-//         // typeorm: 'typeorm',
-//         // 'react-native-sqlite-storage': 'react-native-sqlite-storage',
-//         replicate: 'replicate',
-//         // add others here
-//     };
-//     config.experiments = {
-//       asyncWebAssembly: true,
-//       layers: true,
-//     };
-//     config.output.webassemblyModuleFilename = (isServer ? "../" : "") + "static/wasm/[modulehash].wasm";
-//     if (!isServer) {
-//       config.resolve.fallback = {
-//         ...config.resolve.fallback,
-//         fs: false
-//       };
-//     }
-//     config.plugins.push(
-//       new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
-//         resource.request = resource.request.replace(/^node:/, "");
-//       })
-//     );
-
-//     return config;
-//   },
-// }
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
