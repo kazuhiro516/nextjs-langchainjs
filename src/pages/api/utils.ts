e3294e8c374eb2633ec5ae0439c1ee1cf8c97f6e
hndrr <yozhik20@gmail.com>
2023-04-15 18:10:44 +0900

    add

--- src/pages/api/utils.ts
+++ src/pages/api/utils.ts
@@ -1,10 +1,12 @@
 import { OpenAI } from "langchain/llms/openai";
 
-export const runLlm = async () => {
+export const runLlm = async (prompt: string) => {
   const llm = new OpenAI({
     openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   });
   
-  const res = await llm.call("自己紹介してください");
+  const res = await llm.call(
+    prompt,
+  );
   return res;
 };
