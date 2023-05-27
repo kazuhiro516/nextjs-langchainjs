b8a83a7d34a4f48c72ef3094a1e17cf0f7fb066f
hndrr <yozhik20@gmail.com>
2023-05-27 21:00:26 +0900

    hotfix

--- src/components/Chat/ChatHeader/index.tsx
+++ src/components/Chat/ChatHeader/index.tsx
@@ -1,11 +1,10 @@
 import { Flex, Avatar, AvatarBadge, Text } from "@chakra-ui/react";
 import React from "react";
-import { PodIcon } from "@/components/Svg/PodIcon";
 
 export const ChatHeader = () => {
   return (
     <Flex w="100%">
-      <Avatar size="lg" icon={<PodIcon boxSize="70px" />}>
+      <Avatar size="lg">
         <AvatarBadge boxSize="1.25em" bg="green.500" />
       </Avatar>
       <Flex flexDirection="column" mx="5" justify="center">
